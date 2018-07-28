70b9fab3826bb4c142bf949835768077e8c665ef
panchihpeng <panzhipeng1007@gmail.com>
2018-07-28 16:02:25 +0800

    compact

--- lodash.js
+++ lodash.js
@@ -2,8 +2,20 @@
 function chunck(array, size) {
   let newArray = []
   for (let i = 0; i < array.length; i = i + size) {
-      newArray.push(array.slice(i, i + size))
+    newArray.push(array.slice(i, i + size))
   }
   return newArray
 }
 chunck(['a', 'b', 'c', 'd'], 3)
+
+// compact
+function compact(array) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray
+}
+compact([0, 1, false, 2, '', 3])
