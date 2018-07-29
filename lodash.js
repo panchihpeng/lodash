ca2d226a627d2c69bbdf97311655654e0f473046
panchihpeng <panzhipeng1007@gmail.com>
2018-07-29 14:39:49 +0800

    forEach

--- lodash.js
+++ lodash.js
@@ -1,3 +1,28 @@
+// forEach
+function forEach(array, action) {
+  for (let i = 0; i < array.length; i++) {
+    action(array[i])
+  }
+}
+// sigma
+function sigma(f, start, end) {
+  let sum = 0
+  for (let i = start; i <= end; i++) {
+    // sum = sum + f(i)
+    sum += f(i)
+  }
+  return sum
+}
+// filter
+function filter(array, test) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    if (test(array[i])) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray
+}
 // chunck
 function chunck(array, size) {
   let newArray = []
@@ -19,3 +44,6 @@ function compact(array) {
   return newArray
 }
 compact([0, 1, false, 2, '', 3])
+
+//difference
+function difference() {}
