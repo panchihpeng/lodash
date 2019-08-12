9069758b814214c64a243855de97344e1f19904a
panchihpeng <panzhipeng1007@gmail.com>
2019-08-12 23:13:21 +0800

    practice curry

--- 5---1.js
+++ 5---1.js
@@ -877,6 +877,16 @@ const curry = (fn, len = func.length) => {
   }
 }
 
+// 比较多次接受的参数总数与函数定义时的入参数量，当接受参数的数量大于或等于被 Currying 函数的传入参数数量时，就返回计算结果，否则返回一个继续接受参数的函数。
+function curry (fn, ...args) {
+  if (args.length >= fn.length) {
+    return fn(...args)
+  } else {
+    return curry(fn, ...args, ...args2)
+  }
+}
+
+
 function debounce(f, duration) {
   let timer
 
